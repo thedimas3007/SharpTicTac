ffafa4bb7f8ab0f47131dc6766ec60a34ac91896
thedimas3007 <dima.orischenko@gmail.com>
2021-12-12 21:32:44 +0200

    проверка на занятость клетки

--- SharpTicTac/Form1.cs
+++ SharpTicTac/Form1.cs
@@ -13,10 +13,17 @@ namespace SharpTicTac
     public partial class Form1 : Form
     {
         bool x = true;
-        bool[,] field = new bool[3, 3];
+        int[,] field = new int[3, 3];
         public Form1()
         {
             InitializeComponent();
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    field[x, y] = 0;
+                }
+            }
         }
 
         private void button_Click(object sender, EventArgs e)
@@ -25,19 +32,27 @@ namespace SharpTicTac
             Button button = (Button)sender;
             // невозможно юзать свич из-за ограничений шарпа (кейс ток константы)
             string pChar = x ? "X" : "O";
-            x = !x;
+            int row = 0, col = 0;
+            if (button == button1) { row = 0; col = 0; }
+            else if (button == button2) { row = 0; col = 1; }
+            else if (button == button3) { row = 0; col = 2; }
+            else if (button == button4) { row = 1; col = 0; }
+            else if (button == button5) { row = 1; col = 1; }
+            else if (button == button6) { row = 1; col = 2; }
+            else if (button == button7) { row = 2; col = 0; }
+            else if (button == button8) { row = 2; col = 1; }
+            else if (button == button9) { row = 2; col = 2; }
 
-            if (button == button1) { field[0, 0] = true; }
-            else if (button == button2) { field[0, 1] = true; }
-            else if (button == button3) { field[0, 2] = true; }
-            else if (button == button4) { field[1, 0] = true; }
-            else if (button == button5) { field[1, 1] = true; }
-            else if (button == button6) { field[1, 2] = true; }
-            else if (button == button7) { field[2, 0] = true; }
-            else if (button == button8) { field[2, 1] = true; }
-            else if (button == button9) { field[2, 2] = true; }
-
-            button.Text = pChar;
+            if (field[row, col] == 0)
+            {
+                x = !x;
+                field[row, col] = x ? 1 : 2;
+                button.Text = pChar;
+            }
+            else
+            {
+                MessageBox.Show("Клетка занята!");
+            }
         }
     }
 }
